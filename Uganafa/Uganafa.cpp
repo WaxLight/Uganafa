529b16935bf558d87dbb383907d1293a4b30c995
Waxlight <waxloght@gmail.com>
2025-01-02 14:32:21 +0700

    Добавил header

--- Uganafa/Uganafa.cpp
+++ Uganafa/Uganafa.cpp
@@ -1,6 +1,7 @@
 ﻿// Uganafa.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 #include <iostream>
+#include <SDL.h>
 //позиция объеста В пространстве
 struct Position {
     int x;
