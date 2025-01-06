3416772a003d793e5d6301e035f662ccf07e197c
Waxlight <waxloght@gmail.com>
2025-01-06 17:05:38 +0700

    Подготовил для добавления шрифтов

--- Uganafa/AssetManager.cpp
+++ Uganafa/AssetManager.cpp
@@ -31,4 +31,10 @@ SDL_Texture* AssetManager::GetTexture(std::string id)
 
 void AssetManager::AddFont(std::string id, std::string path, int fontSize)
 {
+	fonts.emplace(id, TTF_OpenFont(path.c_str(), fontSize));
+}
+
+TTF_Font* AssetManager::GetFont(std::string id)
+{
+	return fonts[id];
 }
